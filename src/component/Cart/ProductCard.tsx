7c4b0f126ec14fe7080b1a3ccbdf44a1e1dbeeed
Headoros <djmikeraar@gmail.com>
2022-05-10 13:33:03 +0200

    new

--- src/component/Cart/ProductCard.tsx
+++ src/component/Cart/ProductCard.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import './ProductCard.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { AddProduct, productArray, RemoveAmountOfExisitingProduct } from '../Meal/mealItem';
+import { Product } from '../../interfaces/Product';
 
 interface IProps {
-    id: number;
-    name: string;
-    imgSrc: string;
-    totalPrice: number;
-    amount: number;
-    singlePrice: number;
+    product: Product
 }
 
 interface IState {
-    items: any[];
-    amount: number;
+    product: Product;
 }
 
 class Card extends React.Component<IProps, IState> {
@@ -23,30 +16,13 @@ class Card extends React.Component<IProps, IState> {
         super(props)
 
         this.state = {
-            items: [],
-            amount: 1
+            product: this.props.product
         }
     }
 
-    AddProduct = () => {
-        const count = this.state.amount;
-        this.setState({ amount: count + 1 });
-    }
-
-    RemoveProduct = () => {
-        const count = this.state.amount;
-        if (this.state.amount > 0) {
-            this.setState({ amount: count - 1 });
-        }
-    }
-
-    doMore = () => {
-        this.AddProduct();
-    }
-
-    doLess = () => {
-        this.RemoveProduct();
-    }
+    // ChangeAmount = (product: Product) => {
+    //     this.setState({ cartItems: [...this.state.cartItems, product] });
+    // }
 
     render() {
         return (
@@ -55,37 +31,21 @@ class Card extends React.Component<IProps, IState> {
                     <div className="col-md-2 col-lg-2 col-xl-2">
                         <img
                             alt=''
-                            src={this.props.imgSrc}
+                            src={this.props.product.imgSrc}
                             className="img-fluid rounded-3" />
                     </div>
                     <div className="col-md-3 col-lg-3 col-xl-3">
-                        <h6 >{this.props.name}</h6>
+                        <h6 >{this.props.product.name}</h6>
                     </div>
                     <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                        <button className="btn btn-light" onClick={() => RemoveAmountOfExisitingProduct({
-                            id: this.props.id,
-                            name: this.props.name,
-                            amount: this.props.amount,
-                            singlePrice: this.props.singlePrice,
-                            totalPrice: this.props.totalPrice,
-                            imgSrc: this.props.imgSrc,
-                            price: 0
-                        })}>-</button>
+                        <button className="btn btn-light" onClick={() => { }}>-</button>
 
-                        <input id="form1" min="0" name="quantity" value={this.props.amount} disabled={true} className="form-control form-control-sm-6" />
+                        <input id="form1" min="0" name="quantity" value={this.props.product.amount} disabled={true} className="form-control form-control-sm-6" />
 
-                        <button className="btn btn-light" onClick={() => AddProduct({
-                            id: this.props.id,
-                            name: this.props.name,
-                            amount: this.props.amount,
-                            singlePrice: this.props.singlePrice,
-                            totalPrice: this.props.totalPrice,
-                            imgSrc: this.props.imgSrc,
-                            price: 0
-                        })}>+</button>
+                        <button className="btn btn-light" onClick={() => { }}>+</button>
                     </div>
                     <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                        <h6 className="mb-0">€ {this.props.totalPrice}</h6>
+                        <h6 className="mb-0">€ {this.props.product.totalPrice}</h6>
                     </div>
                 </div>
                 <hr className="my-4" />
